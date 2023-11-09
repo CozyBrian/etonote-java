9ca22e1b9bd3447e8feffa3e96b8966e14d8d453
CozyBrian <dragonbrian33@gmail.com>
2023-11-09 19:13:42 +0000

    Initial commit

---
+++ src/main/java/me/briannewton/etonote/EtonoteApplication.java
@@ -0,0 +1,13 @@
+package me.briannewton.etonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtonoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtonoteApplication.class, args);
+	}
+
+}
